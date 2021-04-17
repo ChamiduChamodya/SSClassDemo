7b50e993a4085e10c144b47a52cac904bb2c1746
Chamidu Chamodya <47558333+ChamiduChamodya@users.noreply.github.com>
2021-04-17 19:04:50 +0530

    Add files via upload

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,32 +8,75 @@ public class Main {
     public static void main(String[] args) throws InputMismatchException {
         // write your code here
 
-//        method 01
-        int x, y;
-        Scanner scanner = new Scanner(System.in);
+//ClassNotFoundException
+//        try {
+//            Class.forName("oracle.jdbc.driver.mysqlDriver");
+//        } catch (ClassNotFoundException e) {
+//            System.out.println(e.toString());
+//        } catch (Exception ex) {
+//            System.out.println("Something went Wrong");
+//        } finally {
+//            System.out.println("Garbage Collection is activated");
+//        }
 
+//CloneNotSupportedException
+        Employee emp1 = new Employee("Rohit");
+        System.out.println(emp1);
         try {
-            System.out.println("Set Value for x:");
-            x = scanner.nextInt();
-            System.out.println("Set Value for y:");
-            y = scanner.nextInt();
-            System.out.println(x + y);
-        } catch (InputMismatchException e) {
-            System.out.println("Make sure the input is an integer values!");
-        } catch (Exception e) {
-            System.out.println("Something went wrong!!Try Again later!");
+            Employee emp2 = (Employee) emp1.clone();
+            System.out.println(emp2);
+        } catch (CloneNotSupportedException e) {
+            System.out.println("Clone Not Supported");
         }
 
-        //method 02 using throws
-        int a, b;
-        Scanner scanner1 = new Scanner(System.in);
-
-        System.out.println("Set Value for x:");
-        a = scanner1.nextInt();
-        System.out.println("Set Value for y:");
-        b = scanner1.nextInt();
-        System.out.println(a + b);
-        System.out.println("Make sure the input is an integer values!");
+//        method 01 using try catch to handel an exception
+//        int x, y;
+//        Scanner scanner = new Scanner(System.in);
+//
+//        try {
+//            System.out.println("Set Value for x:");
+//            x = scanner.nextInt();
+//            System.out.println("Set Value for y:");
+//            y = scanner.nextInt();
+//            System.out.println(x + y);
+//        } catch (InputMismatchException e) {
+//            System.out.println("Make sure the input is an integer values!");
+//        } catch (Exception e) {
+//            System.out.println("Something went wrong!!Try Again later!");
+//        }
+//
+//        //method 02 using throws
+//        int a, b;
+//        Scanner scanner1 = new Scanner(System.in);
+//
+//        System.out.println("Set Value for x:");
+//        a = scanner1.nextInt();
+//        System.out.println("Set Value for y:");
+//        b = scanner1.nextInt();
+//        System.out.println(a + b);
+//        System.out.println("Make sure the input is an integer values!");
 
     }
 }
+
+class Employee {
+
+    private String name;
+
+    public Employee(String name) {
+        super();
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee[Name= " + name + "]";
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+}
+
